35ab912c7dc303993068cb6890b03e9049035a62
Neill Henning <neillhenning@gmail.com>
2018-01-22 22:35:27 +0200

    Example 1.24 if () ... else approach

---
+++ Chapter01/example_1_24_arithmetic/example_1_24_arithmetic.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2018-01-22T22:34:31
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = example_1_24_arithmetic
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ Chapter01/example_1_24_arithmetic/main.cpp
@@ -0,0 +1,33 @@
+#include <QTextStream>
+
+int main()
+{
+    QTextStream cout(stdout);
+
+    double x(1.23), y(4.56), z(7.89);
+    int i(2), j(5), k(7);
+
+    x += y;
+    z *= x;
+
+    cout << "x = " << x << "\tz = " << z
+         << "\nx - z = " << x - z << endl;
+
+    cout << "k / i = " << k / i
+    << "\tk % j = " << k % j << endl ;
+    cout << "i = " << i << "\tj = " << j << "\tk = " << k << endl;
+    cout << "++k / i = " << ++k / i << endl;
+    cout << "i = " << i << "\tj = " << j << "\tk = " << k << endl;
+    cout << "i * j-- = " << i * j-- << endl;
+    cout << "i = " << i << "\tj = " << j << "\tk = " << k << endl;
+
+    cout << "z / j = " << z / j << endl ;
+
+    /* if () ... else approach */
+    if (x * j <= z)
+    cout << x * j << " <= " << z << endl ;
+    else
+    cout << x * j << " > " << z << endl;
+
+    return 0;
+}
