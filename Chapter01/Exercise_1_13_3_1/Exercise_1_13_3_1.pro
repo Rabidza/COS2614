34cb3269f0fdceef43dbc82fdf62bad66e862aa9
Neill Henning <neillhenning@gmail.com>
2018-01-22 23:45:30 +0200

    Done Exercise 1.13.3.1

---
+++ Chapter01/Exercise_1_13_3_1/Exercise_1_13_3_1.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2018-01-22T22:57:00
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Exercise_1_13_3_1
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
---
+++ Chapter01/Exercise_1_13_3_1/main.cpp
@@ -0,0 +1,41 @@
+/*
+1. Write a short program that asks the user to enter a Celsius value and then
+computes the equivalent Fahrenheit temperature. It should use a QInputDialog
+*/
+#include <QtWidgets>
+#include <QApplication>
+#include <QInputDialog>
+#include <QString>
+
+int ConvertToFahrenheit(int degress)
+{
+    return degress * 9.0/5.0 + 32;
+}
+
+int main (int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+    QTextStream cout(stdout);
+    int answer = 0;
+    do {
+        bool ok;
+        double Celsius = QInputDialog::getDouble(0, "Enter Degrees Celsius", "Degrees: ", 0.00, -10000, 10000, 2, &ok);
+        double Fahrenheit;
+        if (ok)
+        {
+            Fahrenheit = ConvertToFahrenheit(Celsius);
+
+            QString response = QString("%1 Degrees Celsius = %2 Degrees Fahrenheit").arg(Celsius).arg(Fahrenheit);
+            answer = QMessageBox::question(0, "Calculate Again?", response, QMessageBox::Yes | QMessageBox::No);
+            ok = false;
+        }
+    } while (answer == QMessageBox::Yes);
+
+    cout << "Celsius\tFahrenheit" << endl;
+    for (int i = 0; i <= 100; i+=5)
+        cout << i << "\t" << ConvertToFahrenheit(i) << endl;
+
+
+
+    return EXIT_SUCCESS;
+}
