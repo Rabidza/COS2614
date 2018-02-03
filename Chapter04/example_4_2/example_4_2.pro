09e35b98cf29b60536ed1929a57af0c7e7d1dd0a
Neill Henning <neillhenning@gmail.com>
2018-02-03 21:33:22 +0200

    Example 4.2 shows a recursive function that uses QDir, QFileInfo, QDirIterator to visit selected items in a directory

---
+++ Chapter04/example_4_2/example_4_2.pro
@@ -0,0 +1,19 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2018-02-03T21:18:15
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = example_4_2
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += \
+    recurseadddir.cpp
---
+++ Chapter04/example_4_2/recurseadddir.cpp
@@ -0,0 +1,40 @@
+#include <QtCore>
+
+void addMp3File(QString path)
+{
+    static QTextStream cout(stdout);
+    cout << path << endl;
+}
+
+void recurseAddDir(QDir d, bool recursive=true, bool symlinks=false)
+{
+    d.setSorting(QDir::Name);
+    QDir::Filters df = QDir::Files | QDir::NoDotAndDotDot;
+    if(recursive) df |= QDir::Dirs;
+    if(not symlinks) df |= QDir::NoSymLinks;
+    QStringList qsl = d.entryList(df, QDir::Name | QDir::DirsFirst);
+    foreach(const QString &entry, qsl)
+    {
+        QFileInfo finfo(d, entry);
+        if(finfo.isDir())
+        {
+            QDir sd(finfo.absoluteFilePath());
+            recurseAddDir(sd);
+        }
+        else
+        {
+            if(finfo.completeSuffix()=="mp3")
+                addMp3File(finfo.absoluteFilePath());
+        }
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    QCoreApplication app(argc, argv);
+    QDir dir = QDir::current();
+    if(app.arguments().size() > 1)
+        dir = app.arguments()[1];
+
+    recurseAddDir(dir);
+}
