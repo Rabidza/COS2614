0bbb910f9a3b59772b27e4cbca323b1748104da3
Neill Henning <neillhenning@gmail.com>
2018-02-04 22:46:04 +0200

    Example 5.8 shows the definitions of the nonmember friend functions. Th ey are defined like ordinary global functions.

--- Chapter05/Example_5_6_to_5_9/complex.cpp
+++ Chapter05/Example_5_6_to_5_9/complex.cpp
@@ -18,3 +18,14 @@ Complex& Complex::operator -=(const Complex& c)
     m_Im -= c.m_Im;
     return *this;
 }
+
+ostream& operator<<(ostream& out, const Complex& c)
+{
+    out << '(' << c.m_Re << ',' << c.m_Im << ')';
+    return out;
+}
+
+Complex operator-(const Complex& c1, const Complex& c2)
+{
+    return Complex(c1.m_Re - c2.m_Re, c1.m_Im - c2.m_Im);
+}
