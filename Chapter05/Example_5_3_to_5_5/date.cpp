be239238787cd42a6970067ca2562b331de2fd63
Neill Henning <neillhenning@gmail.com>
2018-02-04 22:26:07 +0200

    Th e constructor definition shown in Example 5.4 looks the same as usual; no default arguments need to be specified there.

---
+++ Chapter05/Example_5_3_to_5_5/date.cpp
@@ -0,0 +1,21 @@
+#include <QDate>
+#include "date.h"
+#include <iostream>
+
+Date::Date(int d, int m, int y)
+    : m_Day(d), m_Month(m), m_Year(y)
+{
+    static QDate currentDate = QDate::currentDate();
+
+    if(m_Day == 0) m_Day = currentDate.day();
+    if(m_Month== 0) m_Month = currentDate.month();
+    if(m_Year == 0) m_Year = currentDate.year();
+}
+
+void Date::display(bool eoln) const
+{
+    using namespace std;
+    cout << m_Year << "/" << m_Month << '/' << m_Day;
+    if(eoln)
+        cout << endl;
+}
--- Chapter05/Example_5_3_to_5_5/date.h
+++ Chapter05/Example_5_3_to_5_5/date.h
@@ -4,7 +4,7 @@
 class Date
 {
 public:
-    Date(int d = 0; int m = 0, int y = 0);
+    Date(int d = 0, int m = 0, int y = 0);
     void display(bool eoln = true) const;
 private:
     int m_Day, m_Month, m_Year;
