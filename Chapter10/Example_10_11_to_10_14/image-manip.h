a981c8fa4fbe7f50d0131cf9c1eaff9f2f99d0b6
Neill Henning <neillhenning@gmail.com>
2018-04-08 16:36:46 +0200

    changed formatting

--- Chapter10/Example_10_11_to_10_14/image-manip.h
+++ Chapter10/Example_10_11_to_10_14/image-manip.h
@@ -4,12 +4,12 @@
 #include <QUndoCommand>
 #include <QImage>
 
-//start
-class AdjustColors : public QUndoCommand {
+class AdjustColors : public QUndoCommand
+{
 public:
    AdjustColors(QImage& img, double radj, double gadj, double badj)
-     : m_Image(img), m_Saved(img.size(), img.format()), m_RedAdj(radj),
-     m_GreenAdj(gadj), m_BlueAdj(badj)   {setText("adjust colors"); }
+       : m_Image(img), m_Saved(img.size(), img.format()), m_RedAdj(radj),
+         m_GreenAdj(gadj), m_BlueAdj(badj)   {setText("adjust colors"); }
    virtual void undo();
    virtual void redo();
 private:
@@ -20,15 +20,15 @@ private:
 
 };
 
-class MirrorPixels : public QUndoCommand {
+class MirrorPixels : public QUndoCommand
+{
 public:
    virtual void undo();
    virtual void redo();
 //end
     MirrorPixels(QImage& img, bool horiz): m_Image(img), m_Saved(img.size(),
-      img.format()), m_Horizontal(horiz) {
-      setText(QString("mirror pixels around %1 line").arg(horiz
-         ?"horizontal":"vertical"));
+                                                                 img.format()), m_Horizontal(horiz) {
+        setText(QString("mirror pixels around %1 line").arg(horiz ? "horizontal" : "vertical"));
    }
 private:
    QImage& m_Image;
